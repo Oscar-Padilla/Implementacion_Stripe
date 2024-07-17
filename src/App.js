a800df6ac22542ebd5d2960b09e9983349062d08
Oscar <oscarbarcenas417@gmail.com>
2024-07-16 22:58:52 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import img from './porsche-thumbwhite.webp';
+import { CardFooter } from 'react-bootstrap';
+
+function Compra() {
+  const handleButtonClick = () => {
+    window.open('https://buy.stripe.com/test_00gaHLfYwbXweXK288', '_blank');
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: 'gray' }}>
+      <Card border="dark" style={{ width: '18rem', borderRadius: '15px' }}>
+        <Card.Img variant="top" src={img} style={{borderRadius: '15px'}}/>
+        <Card.Body>
+          <Card.Title>911 GT3 RS</Card.Title>
+          <Card.Text>
+            Aún más deportivo. <br/>
+            USD 270,588.64
+          </Card.Text>
+          <CardFooter style={{backgroundColor: 'white'}} >
+            <div className="d-grid gap-2">
+              <Button onClick={handleButtonClick} variant="dark">Comprar</Button>
+            </div>
+          </CardFooter>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default Compra;
+
+
